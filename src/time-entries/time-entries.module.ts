beb75013918e7465fd2d530b2959617ac2e5994e
automationconsultants8-cmd <automationconsultants8@gmail.com>
2026-01-02 20:03:40 -0500

    Update time-entries.module.ts

--- src/time-entries/time-entries.module.ts
+++ src/time-entries/time-entries.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
-import { TimeEntriesService } from './time-entries.service';
 import { TimeEntriesController } from './time-entries.controller';
+import { TimeEntriesService } from './time-entries.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { JobsModule } from '../jobs/jobs.module';
 import { AwsModule } from '../aws/aws.module';
 import { EmailModule } from '../email/email.module';
+import { AuditModule } from '../audit/audit.module';
 import { BreakComplianceModule } from '../break-compliance/break-compliance.module';
 
 @Module({
-  imports: [PrismaModule, JobsModule, AwsModule, EmailModule],
+  imports: [PrismaModule, JobsModule, AwsModule, EmailModule, AuditModule, BreakComplianceModule],
   controllers: [TimeEntriesController],
   providers: [TimeEntriesService],
+  exports: [TimeEntriesService],
 })
 export class TimeEntriesModule {}
