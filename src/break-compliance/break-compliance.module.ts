e9a9ee5f2217dfd3db6b1c38f4953207e34ce70b
automationconsultants8-cmd <automationconsultants8@gmail.com>
2026-01-02 19:57:09 -0500

    Update break-compliance.module.ts

--- src/break-compliance/break-compliance.module.ts
+++ src/break-compliance/break-compliance.module.ts
@@ -1,1 +1,13 @@
+import { Module } from '@nestjs/common';
+import { BreakComplianceService } from './break-compliance.service';
+import { BreakComplianceController } from './break-compliance.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+import { AuditModule } from '../audit/audit.module';
 
+@Module({
+  imports: [PrismaModule, AuditModule],
+  controllers: [BreakComplianceController],
+  providers: [BreakComplianceService],
+  exports: [BreakComplianceService],
+})
+export class BreakComplianceModule {}
