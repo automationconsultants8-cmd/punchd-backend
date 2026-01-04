da5a1b99696ca89f1cad6ba11ae478f52a099bdd
automationconsultants8-cmd <automationconsultants8@gmail.com>
2026-01-04 00:35:11 -0500

    Create update-company.dto.ts

---
+++ src/company/dto/update-company.dto.ts
@@ -0,0 +1,34 @@
+import { IsString, IsOptional } from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+
+export class UpdateCompanyDto {
+  @ApiPropertyOptional()
+  @IsString()
+  @IsOptional()
+  name?: string;
+
+  @ApiPropertyOptional()
+  @IsString()
+  @IsOptional()
+  address?: string;
+
+  @ApiPropertyOptional()
+  @IsString()
+  @IsOptional()
+  city?: string;
+
+  @ApiPropertyOptional()
+  @IsString()
+  @IsOptional()
+  state?: string;
+
+  @ApiPropertyOptional()
+  @IsString()
+  @IsOptional()
+  zip?: string;
+
+  @ApiPropertyOptional()
+  @IsString()
+  @IsOptional()
+  timezone?: string;
+}
