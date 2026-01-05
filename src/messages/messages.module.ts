c808fe2023a9ab75b8b9410b5ec3c429afa10305
automationconsultants8-cmd <automationconsultants8@gmail.com>
2026-01-05 15:54:06 -0500

    Create messages.module.ts

---
+++ src/messages/messages.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { MessagesController } from './messages.controller';
+import { MessagesService } from './messages.service';
+import { PrismaModule } from '../prisma/prisma.module';
+import { AuditModule } from '../audit/audit.module';
+
+@Module({
+  imports: [PrismaModule, AuditModule],
+  controllers: [MessagesController],
+  providers: [MessagesService],
+  exports: [MessagesService],
+})
+export class MessagesModule {}
