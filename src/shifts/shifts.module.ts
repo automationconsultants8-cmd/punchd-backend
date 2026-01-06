7b9a3456f38af52e02c26d91f802ea48e8ca8c76
automationconsultants8-cmd <automationconsultants8@gmail.com>
2026-01-06 01:39:30 -0500

    Update shifts.module.ts

--- src/shifts/shifts.module.ts
+++ src/shifts/shifts.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ShiftsController } from './shifts.controller';
 import { ShiftsService } from './shifts.service';
+import { ShiftsController } from './shifts.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+import { AuditModule } from '../audit/audit.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
+  imports: [PrismaModule, AuditModule, NotificationsModule],
   controllers: [ShiftsController],
   providers: [ShiftsService],
   exports: [ShiftsService],
