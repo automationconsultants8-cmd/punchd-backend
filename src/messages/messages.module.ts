0d29815e1a6c8b7b3c53bc30fcc65cff6d1db79b
automationconsultants8-cmd <automationconsultants8@gmail.com>
2026-01-06 01:06:59 -0500

    Update messages.module.ts

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { MessagesController } from './messages.controller';
 import { MessagesService } from './messages.service';
+import { MessagesController } from './messages.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuditModule } from '../audit/audit.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
-  imports: [PrismaModule, AuditModule],
+  imports: [PrismaModule, AuditModule, NotificationsModule],
   controllers: [MessagesController],
   providers: [MessagesService],
   exports: [MessagesService],
