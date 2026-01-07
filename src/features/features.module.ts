ab2aa7dee8bc215bad445b1bc489f5bf654926ab
automationconsultants8-cmd <automationconsultants8@gmail.com>
2026-01-06 22:07:58 -0500

    Update features.module.ts

--- src/features/features.module.ts
+++ src/features/features.module.ts
@@ -1,9 +1,11 @@
 import { Module, Global } from '@nestjs/common';
-import { FeatureGuard } from './feature.guard';
 import { FeaturesController } from './features.controller';
+import { FeatureGuard } from './feature.guard';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Global()
 @Module({
+  imports: [PrismaModule],
   controllers: [FeaturesController],
   providers: [FeatureGuard],
   exports: [FeatureGuard],
