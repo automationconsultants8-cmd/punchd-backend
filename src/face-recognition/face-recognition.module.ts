9dceaad2bc0f9c486f4a7fa113af23762d1927d6
automationconsultants8-cmd <automationconsultants8@gmail.com>
2026-01-06 22:24:06 -0500

    Update face-recognition.module.ts

--- src/face-recognition/face-recognition.module.ts
+++ src/face-recognition/face-recognition.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FaceRecognitionService } from './face-recognition.service';
+import { PrismaModule } from '../prisma/prisma.module';
+import { AwsModule } from '../aws/aws.module';
 
 @Module({
+  imports: [PrismaModule, AwsModule],
   providers: [FaceRecognitionService],
   exports: [FaceRecognitionService],
 })
