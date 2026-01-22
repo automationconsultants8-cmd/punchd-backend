0fb3d91e09b0fb5de592a14364b46735a0546365
automationconsultants8-cmd <automationconsultants8@gmail.com>
2026-01-21 21:48:44 -0500

    Update users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { AwsModule } from '../aws/aws.module';
+import { StripeModule } from '../stripe/stripe.module';
 
 @Module({
-  imports: [AwsModule],
+  imports: [AwsModule, StripeModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
